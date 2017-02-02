f382783c9874a43d66a53e652b918521bd20edff
nissim <nissim.kurle@tekolutions.com>
2017-02-02 11:42:59 +0530

    initial commit

--- app/src/main/java/com/prodyogic/cropimage/MainActivity.java
+++ app/src/main/java/com/prodyogic/cropimage/MainActivity.java
@@ -1,8 +1,10 @@
 package com.prodyogic.cropimage;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,13 +15,9 @@ import com.soundcloud.android.crop.Crop;
 
 import java.io.File;
 
-
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1000;
-    private static final int SELECT_FILE = 1001;
-    private final int RESULT_CROP = 400;
     private ImageView mCropImageView;
-    private Uri picUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +40,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_crop) {
+        int id = item.getItemId();
+        if (id == R.id.action_crop) {
             mCropImageView.setImageDrawable(null);
             Crop.pickImage(this);
+            return true;
+
+        }
+        if (id == R.id.action_select) {
+            getPickImageChooserIntent();
             return true;
         }
 
         return (super.onOptionsItemSelected(item));
+    }
+
+    private void getPickImageChooserIntent() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePicture, CAMERA_REQUEST);
     }
 
     @Override
@@ -57,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
             beginCrop(result.getData());
         } else if (requestCode == Crop.REQUEST_CROP) {
             handleCrop(resultCode, result);
+        } else if (requestCode == CAMERA_REQUEST) {
+            Bitmap photo = (Bitmap) result.getExtras().get("data");
+            mCropImageView.setImageBitmap(photo);
         }
     }
 
@@ -72,54 +84,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
-
 }
 
-   /*
-
-    public void onLoadImageClick(View view) {
-        startActivityForResult(getPickImageChooserIntent(), 200);
-    }
-
-    public Intent getPickImageChooserIntent() {
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-
-// collect all gallery intents
-        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        galleryIntent.setType("image/*");
-        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);//
-        startActivityForResult(Intent.createChooser(galleryIntent, "Select File"), SELECT_FILE);
-
-// Create a chooser from the main  intent
-        Intent chooserIntent = Intent.createChooser(galleryIntent, "Select source");
-        return chooserIntent;
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            mCropImageView.setImageBitmap(photo);
-        }
-        if (requestCode == SELECT_FILE) {
-            onSelectFromGalleryResult(data);
-        }
-    }
-
-    private void onSelectFromGalleryResult(Intent data) {
-
-        Bitmap bm = null;
-        if (data != null) {
-            try {
-                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        mCropImageView.setImageBitmap(bm);
-    }
-
-    private void performCrop() {
-
-    }
-}*/
